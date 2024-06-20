85daa1835b2c877c2c7904c2b9d0159d03540dd7
Matthiew Purple <matthiewpurple@hotmail.com>
2024-06-20 09:58:25 -0400

    added discount every 10 percents variant

--- CompendiumDiscounts.cs
+++ CompendiumDiscounts.cs
@@ -4,15 +4,12 @@ using Il2Cpp;
 using compendium_discounts;
 using System.Text.RegularExpressions;
 
-[assembly: MelonInfo(typeof(CompendiumDiscounts), "Compendium Discounts (Very generous)", "1.0.0", "Matthiew Purple but mostly Zephhyr let's be honest")]
+[assembly: MelonInfo(typeof(CompendiumDiscounts), "Compendium Discounts (Discount every 10 percents)", "1.0.0", "Matthiew Purple but mostly Zephhyr let's be honest")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace compendium_discounts;
 public class CompendiumDiscounts : MelonMod
 {
-    public const float limitDiscount = 90f; // What the discount scales towards the closer you get to 100% compendium completion
-    public const float finalDiscount = 100f; // Discount once the compendium is completed
-
     public static short currentRecord;
 
     // After replacing localized text
@@ -25,7 +22,7 @@ public class CompendiumDiscounts : MelonMod
             if (__result.Contains("<SP7><FO1>It will cost <CO4>") && __result.Contains("Are you okay with that?"))
             {
                 var macca = int.Parse(string.Concat(__result.Replace("<SP7><FO1>It will cost <CO4>", string.Empty).Where(char.IsNumber)));
-                macca = (int)(macca * Utility.GetDiscountFactor(limitDiscount, finalDiscount));
+                macca = macca * Utility.GetDiscountFactor();
                 __result = "<SP7><FO1>It will cost <CO4>" + macca + " Macca. <CO0>Are you okay with that?";
             }
 
@@ -33,7 +30,7 @@ public class CompendiumDiscounts : MelonMod
             else if (__result.Contains("<SP7><FO1>It will cost <CO4>") && __result.Contains("But it seems you don't have enough."))
             {
                 var macca = int.Parse(string.Concat(__result.Replace("<SP7><FO1>It will cost <CO4>", string.Empty).Where(char.IsNumber)));
-                macca = (int)(macca * Utility.GetDiscountFactor(limitDiscount, finalDiscount));
+                macca = macca * Utility.GetDiscountFactor();
                 __result = "<SP7><FO1>It will cost <CO4>" + macca + " Macca... <CO0>But it seems you don't have enough.";
             }
         }
@@ -55,7 +52,7 @@ public class CompendiumDiscounts : MelonMod
             var pelem = dds3GlobalWork.DDS3_GBWK.encyc_record.pelem[pwork.recindex];
 
             // Get discounted price for that summon
-            pwork.mak = (int)(pwork.mak * Utility.GetDiscountFactor(limitDiscount, finalDiscount));
+            pwork.mak *= Utility.GetDiscountFactor();
 
             // If enough macca post-discount but not pre-discount (and stock not full and not already in stock and something idk)
             if (__result == 0 && dds3GlobalWork.DDS3_GBWK.maka >= pwork.mak && datCalc.datCheckStockFull() == 0 && datCalc.datSearchDevilStock(pelem.id) == -1 && pwork.flags == 80)
@@ -81,7 +78,7 @@ public class CompendiumDiscounts : MelonMod
                 var statTotal = fclEncyc.GetDevilParam(pelem, 0) + fclEncyc.GetDevilParam(pelem, 2) + fclEncyc.GetDevilParam(pelem, 3) + fclEncyc.GetDevilParam(pelem, 4) + fclEncyc.GetDevilParam(pelem, 5);
                 
                 // Sumonning price forumale with applied discount
-                var price = (((statTotal * statTotal) / 20) * 100) * Utility.GetDiscountFactor(limitDiscount, finalDiscount);
+                var price = (((statTotal * statTotal) / 20) * 100) * Utility.GetDiscountFactor();
 
                 // If enough macca post-discount but not pre-discount (and stock not full and not already in stock and something idk)
                 if (dds3GlobalWork.DDS3_GBWK.maka >= price && datCalc.datCheckStockFull() == 0 && datCalc.datSearchDevilStock(pelem.id) == -1)
@@ -101,7 +98,7 @@ public class CompendiumDiscounts : MelonMod
             if (__instance.transform.GetParent().name.Contains("dlistsum_row0"))
             {
                 // Apply discount on displayed prices
-                val = (int)(val * Utility.GetDiscountFactor(limitDiscount, finalDiscount));
+                val *= Utility.GetDiscountFactor();
             }
         }
     }
@@ -109,22 +106,11 @@ public class CompendiumDiscounts : MelonMod
     private class Utility
     {
         // Discount calculator
-        public static float GetDiscountFactor(float limitDiscount, float finalDiscount)
+        public static int GetDiscountFactor()
         {
             int compendiumProgress = fclEncyc.fclEncycGetRatio2();
-            float discountFactor;
 
-            if (compendiumProgress < 100)
-            {
-                discountFactor = 1 - (compendiumProgress * limitDiscount) / (100f * 100f);
-            }
-            else
-            {
-                discountFactor = 1 - (compendiumProgress * finalDiscount) / (100f * 100f);
-                discountFactor *= 2; // To counteract the 50% discount of the vanilla game when the compendium is completed
-            }
-
-            return discountFactor;
+            return compendiumProgress / 10;
         }
     }
 }
